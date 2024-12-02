88aaec6cff3312b016c6f86c74b45baa5cedf9a5
muItilingualism <sebhalfdan@gmail.com>
2024-12-02 22:26:22 +0100

    2024 day2 part2

--- 2024/day2/solution.fsx
+++ 2024/day2/solution.fsx
@@ -31,3 +31,22 @@ let safeReports = List.ofSeq input
                   |> List.map splitBySpaces
                   |> List.filter isSafeReport
                   |> List.length;;
+
+printfn "Part 1 result: %d" safeReports;;
+
+//insane strat incoming
+let removeOneElement lst =
+    [for i in 0 .. List.length lst - 1 -> List.take i lst @ List.skip (i + 1) lst]
+
+let isLineSafe line =
+    let numbers = splitBySpaces line
+    let permutations = removeOneElement numbers
+    List.exists isSafeReport (numbers :: permutations)
+
+let safeReportsDampened = 
+    input
+    |> Seq.filter isLineSafe
+    |> Seq.length
+
+printfn "Part 2 result: %d" safeReportsDampened
+//god forgive me
